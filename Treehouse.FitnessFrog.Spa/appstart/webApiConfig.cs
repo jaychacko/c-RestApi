d2231dd4a4dff7ab3b7c4fa3e0c1f1911d7fb72d
jaychacko <chackojayui@gmail.com>
2018-04-25 17:49:03 -0500

    api class

---
+++ Treehouse.FitnessFrog.Spa/appstart/webApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace Treehouse.FitnessFrog.Spa
+{
+    public static class webApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+        }
+    }
+}
